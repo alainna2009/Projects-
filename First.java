d20008b0c22339efa529bd3c4a1181d2c2708a51
Alainna Ellington <elllington_alainna@students.columbusstate.edu>
2026-08-21 22:07:17 -0400

    First commit

---
+++ First.java
@@ -0,0 +1,12 @@
+// Fist.java
+// name: Alainna Ellington
+//Assignment 1 : hola munod + career info 
+
+public class First{
+    public static void main(String[]args){
+        System.out.println("Hola mundo!");
+        System.out.println("Job title: Information security analyst.");
+        System.out.println("Why: Strong job growth, hands-on problem solving, and it lines up with my cybersecurity coursework.");
+        System.out.println("Median salary: $124,910");
+    }
+}
